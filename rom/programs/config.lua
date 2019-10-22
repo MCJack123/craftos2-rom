ad26d3952ca02d38d4a64c36ff271c84888ee78c
MCJack123 <jackbruienne@yahoo.com>
2019-10-22 09:51:29 -0400

    Changed config.getType return values

--- rom/programs/config.lua
+++ rom/programs/config.lua
@@ -11,9 +11,9 @@ elseif args[1] == "get" and args[2] ~= nil then
     else print(tostring(setting)) end
 elseif args[1] == "set" and args[3] ~= nil then
     local type = config.getType(args[2])
-    if type == 0 then config.set(args[2], args[3] == "true" or args[3] == "1")
-    elseif type == 1 then config.set(args[2], args[3])
-    elseif type == 2 then config.set(args[2], tonumber(args[3]))
+    if type == "boolean" then config.set(args[2], args[3] == "true" or args[3] == "1")
+    elseif type == "string" then config.set(args[2], args[3])
+    elseif type == "number" then config.set(args[2], tonumber(args[3]))
     else error("Unknown setting " .. args[2]) end
 else
     term.setTextColor(colors.red)
