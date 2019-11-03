7c6f42baa900c0b73f7fbbefed41efff9feef6d8
MCJack123 <jackbruienne@yahoo.com>
2019-11-02 22:54:30 -0400

    Added some stuff

--- rom/programs/config.lua
+++ rom/programs/config.lua
@@ -11,6 +11,7 @@ elseif args[1] == "get" and args[2] ~= nil then
     else print(tostring(setting)) end
 elseif args[1] == "set" and args[3] ~= nil then
     local type = config.getType(args[2])
+    if args[2] == "mount_mode" and _G.type(args[3]) == "string" then type = "string" end
     if type == "boolean" then config.set(args[2], args[3] == "true" or args[3] == "1")
     elseif type == "string" then config.set(args[2], args[3])
     elseif type == "number" then config.set(args[2], tonumber(args[3]))
