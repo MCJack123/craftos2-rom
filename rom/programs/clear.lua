8d3456ffe4b952db5e2814137ca18f5cc622c447
JackMacWindows <jackmacwindowslinux@gmail.com>
2019-12-29 05:56:04 -0500

    Update clear.lua

--- rom/programs/clear.lua
+++ rom/programs/clear.lua
@@ -1,6 +1,8 @@
-term.setGraphicsMode( true )
-term.clear()
-term.setGraphicsMode( false )
+if term.setGraphicsMode then
+    term.setGraphicsMode( true )
+    term.clear()
+    term.setGraphicsMode( false )
+end
 term.clear()
 term.setCursorPos( 1, 1 )
 for i = 0, 15 do term.setPaletteColor( 2^i, term.nativePaletteColor( 2^i ) ) end
