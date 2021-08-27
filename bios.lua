3a3a4cb6fa9c4bcd9127a85a862a0c33540bac68
MCJack123 <jackmacwindowslinux@gmail.com>
2021-08-26 23:17:36 -0400

    CC:T 1.98.2 FPR

--- bios.lua
+++ bios.lua
@@ -821,6 +821,7 @@ if http then
     local nativeCheckURL = http.checkURL
     http.checkURLAsync = nativeCheckURL
     http.checkURL = function(_url)
+        expect(1, _url, "string")
         local ok, err = nativeCheckURL(_url)
         if not ok then return ok, err end
 
--- rom/modules/main/cc/strings.lua
+++ rom/modules/main/cc/strings.lua
@@ -2,7 +2,7 @@
 --
 -- @see textutils For additional string related utilities.
 
-local expect = require "cc.expect".expect
+local expect = (require and require("cc.expect") or dofile("rom/modules/main/cc/expect.lua")).expect
 
 --[[- Wraps a block of text, so that each line fits within the given width.
 
